c990f5c90c507a85ce2189e6e8c19071b2591a15
iamprashu <iamprashu.gs@gmail.com>
2025-07-06 13:49:39 +0530

    App And Main Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ClerkWithRouter } from "./config/clerkConfig";
 import AppRoutes from "./routes/AppRoutes";
 import { Toaster } from "react-hot-toast";
-import { UIProvider } from "./contexts/uiContext";
+import { UIProvider, useUI } from "./contexts/uiContext";
 
 export default function App() {
   return (
